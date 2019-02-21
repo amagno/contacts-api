8de82732071131187c68750809a2587035af5594
alexandre <amagnomonteiro@gmail.com>
2019-02-21 15:05:09 -0300

    required e-mail on viewmodel

--- ViewModels/ContactViewModel.cs
+++ ViewModels/ContactViewModel.cs
@@ -12,6 +12,7 @@ namespace ContactsAPI.ViewModels
         [Required]
         [MinLength(3)]
         public string LastName { get; set; }
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
